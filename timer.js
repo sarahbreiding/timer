057a0db1f4636effd6ade037fa89219d4b479a4b
Sarah Breiding <breiding.sarah@gmail.com>
2016-06-15 08:18:12 -0400

    add input field for time

--- timer.js
+++ timer.js
@@ -1,8 +1,7 @@
-var userWorkTime = $('#work-time-input').val();
-console.log(userWorkTime);
-var userBreakTime = $('#break-time-input').val();
-var msLeft = userWorkTime * 60000;
-var msBreakLeft = userBreakTime * 60000 + msLeft;
+var userWorkTime = $('#work-time-input').val() * 60000;
+var userBreakTime = $('#break-time-input').val() * 60000 + userWorkTime;
+// var userWorkTime = userWorkTime * 60000;
+// var userBreakTime = userBreakTime * 60000 + userWorkTime;
 var previousTime;
 $('.pauseButton').hide();
 $('.resetButton').hide();
@@ -10,43 +9,44 @@ var isWorkPaused = false;
 var isBreakPaused = false;
 
 function getTimeRemaining(){
-  var currentTime = Date.now();
+  // currentTime = Date.now();
+  currentTime = new Date().getTime();
   var timeElapsed = currentTime - previousTime;
   previousTime = currentTime;
-  msLeft = msLeft - timeElapsed;
-  msBreakLeft = msBreakLeft - timeElapsed;
-  var seconds = Math.floor((msLeft/1000) % 60);
-  var minutes = Math.floor((msLeft/1000/60) % 60);
-  var breakSeconds = Math.floor((msBreakLeft/1000) % 60)
-  var breakMinutes = Math.floor((msBreakLeft/1000/60) % 60);
+  userWorkTime = userWorkTime - timeElapsed;
+  userBreakTime = userBreakTime - timeElapsed;
+  var seconds = Math.floor((userWorkTime/1000) % 60);
+  var minutes = Math.floor((userWorkTime/1000/60) % 60);
+  var breakSeconds = Math.floor((userBreakTime/1000) % 60)
+  var breakMinutes = Math.floor((userBreakTime/1000/60) % 60);
   return {
-    'total': msLeft,
+    'total': userWorkTime,
     'minutes': minutes,
     'seconds': seconds,
-    'breakTotal': msBreakLeft,
+    'breakTotal': userBreakTime,
     'breakSeconds': breakSeconds,
     'breakMinutes': breakMinutes
   };
 }
 
-var oval = [Math.PI/8, Math.PI/4, 3*Math.PI/8, Math.PI/2, 5*Math.PI/8, 3*Math.PI/4, 7*Math.PI/8, Math.PI, 9*Math.PI/8, 5*Math.PI/4, 11*Math.PI/8, 3*Math.PI/2, 13*Math.PI/8, 7*Math.PI/4, 15*Math.PI/8, 2*Math.PI];
-var ovalSegments = [];
-var segment = msLeft/16;
-for (var i = 15; i >= 0; i--){
-    ovalSegments.push((Math.ceil(segment * i/1000))*1000);
-}
+// var oval = [Math.PI/8, Math.PI/4, 3*Math.PI/8, Math.PI/2, 5*Math.PI/8, 3*Math.PI/4, 7*Math.PI/8, Math.PI, 9*Math.PI/8, 5*Math.PI/4, 11*Math.PI/8, 3*Math.PI/2, 13*Math.PI/8, 7*Math.PI/4, 15*Math.PI/8, 2*Math.PI];
+// var ovalSegments = [];
+// var segment = userWorkTime/16;
+// for (var i = 15; i >= 0; i--){
+//     ovalSegments.push((Math.ceil(segment * i/1000))*1000);
+// }
 //console.log(ovalSegments);
 
 function createClock(){
   var timeinterval = setInterval(function(){
     var t = getTimeRemaining();
-    console.log(t);
+    //console.log(t);
     $('.workClock').html(t.minutes + ':' + t.seconds);
-    for (var i = 0; i < ovalSegments.length; i++){
-      if (t.total <= ovalSegments[i]){
-        drawCircle(oval[i], 'workCircle');
-      }
-    }
+    // for (var i = 0; i < ovalSegments.length; i++){
+    //   if (t.total <= ovalSegments[i]){
+    //     drawCircle(oval[i], 'workCircle');
+    //   }
+    // }
     if (isWorkPaused){
       $('.workClock').html(t.minutes + ':' + t.seconds);
       clearInterval(timeinterval);
@@ -56,10 +56,10 @@ function createClock(){
       clearInterval(timeinterval);
     } else if (t.breakSeconds <= 0 && t.breakTotal <= 0){
         $('.breakClock').html('0:00');
-        msLeft = userWorkTime * 60000;
-        msBreakLeft = userBreakTime * 60000 + msLeft;
-        previousTime = Date.now();
-        t = getTimeRemaining();
+        userWorkTime = $('#work-time-input').val() * 60000;
+        userBreakTime = $('#break-time-input').val() * 60000 + userWorkTime;
+        //previousTime = Date.now();
+        //t = getTimeRemaining();
         $('.workClock').html(t.minutes + ':' + t.seconds);
      } else if (t.seconds <= 0 && t.total <= 0){
       $('.workClock').html('0:00');
@@ -68,7 +68,7 @@ function createClock(){
   //var canvas = document.getElementById('breakCircle');
       //var ctx = canvas.getContext("2d");
       //ctx.clearRect(0, 0, 500, 500);
-      //var breaktime = new Date(new Date().getTime() + userBreaktime*60000);
+      //var breaktime = new Date(new Date().getTime() + userBreaktime);
       //createBreakClock('.breakClock', 'breakCircle');
      $('.resetButton').click(function(){
       clearInterval(timeinterval);
@@ -78,11 +78,12 @@ function createClock(){
       $('.pauseButton').hide();
       $('.startButton').show();
     })
-  },1000);
+  },10);
 }
 
 $('.startButton').click(function(){
-  previousTime = Date.now();
+  //previousTime = Date.now();
+  previousTime = new Date().getTime();
   if (isWorkPaused) {
     isWorkPaused = false;
     createClock();
@@ -98,7 +99,7 @@ $('.startButton').click(function(){
 })
 
 $('.pauseButton').click(function(){
-  if (msLeft > 0){
+  if (userWorkTime > 0){
     isWorkPaused = true;
   } else {
     isBreakPaused = true;
@@ -108,22 +109,45 @@ $('.pauseButton').click(function(){
   $('.pauseButton').hide();
 })
 
-
-//on click get the id of the closest input
-
 $('.incButton').click(function(){
   var $button = $(this);
   var inputId = $button.closest('div.form').find('input').attr('id');
   var oldValue = $button.closest('div.form').find('input').val();
-  console.log(oldValue);
+  var newVal;
   if ($button.text() === '+'){
-    var newVal = parseFloat(oldValue) + 1;
+    newVal = parseFloat(oldValue) + 1;
   } else {
     if (oldValue > 0) {
-      var newVal = parseFloat(oldValue) - 1;
+      newVal = parseFloat(oldValue) - 1;
     } else {
       newVal = 0;
     }
   }
-  $button.parent().find("input").val(newVal);
+  $button.parent().find('input').val(newVal);
+  if (inputId === 'work-time-input'){
+    userWorkTime = newVal * 60000;
+  }
+  else if (inputId === 'break-time-input'){
+    userBreakTime = newVal * 60000;
+  }
 })
+
+$('#work-time-input').change(function(){
+  userWorkTime = $(this).val() * 60000;
+});
+
+$('#break-time-input').change(function(){
+  userBreakTime = $(this).val() * 60000 + userWorkTime;
+})
+
+//on change of the input change userWorkTime and userBreakTime
+
+function drawCircle(angle, id) {
+    var mainCanvas = document.getElementById(id);
+    var mainContext = mainCanvas.getContext('2d');
+    mainContext.beginPath();
+    mainContext.arc(200, 200, 150, 0, angle, false);
+    mainContext.lineWidth = 5;
+    mainContext.strokeStyle = '#66CC01';
+    mainContext.stroke();
+  }
